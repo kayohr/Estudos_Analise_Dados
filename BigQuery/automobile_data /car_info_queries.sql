5fd69318c83636833180c9ed4640fdbc4ada41f9
kayo <kayohramos@gmail.com>
2024-05-23 13:48:55 -0300

    query select_update

--- BigQuery/automobile_data /car_info_queries.sql
+++ BigQuery/automobile_data /car_info_queries.sql
@@ -1,9 +1,11 @@
+-- Seleção de Tipos de Combustível Distintos
 SELECT DISTINCT fuel_type FROM cars.car_info;
 """
 Esta query seleciona todos os tipos de combustível (fuel_type) presentes na tabela car_info.
 palavra-chave DISTINCT para garantir que apenas valores únicos sejam retornados.
 """
 
+-- Cálculo de Comprimentos Mínimo e Máximo
 SELECT MIN(length) AS min_length, MAX(length) AS max_length FROM cars.car_info;
 
 """
--- BigQuery/automobile_data /select_update.sql
+++ BigQuery/automobile_data /select_update.sql
@@ -2,18 +2,18 @@
 SELECT * FROM cars.car_info
 WHERE num_of_doors IS NULL;
 
-"""Seleciona todos os registros da tabela car_info onde a coluna num_of_doors é NULL."""
-
+"""
+Seleciona todos os registros da tabela car_info onde a coluna num_of_doors é NULL.
+"""
 
 -- Atualização de num_of_doors para Carros Específicos
 UPDATE cars.car_info 
 SET num_of_doors = "four" 
-WHERE make = "dodge" AND fuel_type = "gas" AND body_style = "sedan";
-
+WHERE make = "mazda" AND fuel_type = "gas" AND body_style = "sedan";
 
 """
 Atualiza a coluna num_of_doors para o valor "four" para todos os registros na tabela car_info que atendem aos seguintes critérios:
-A coluna make é igual a "dodge".
+A coluna make é igual a "mazda".
 A coluna fuel_type é igual a "gas".
 A coluna body_style é igual a "sedan".
 """
