675e765f628f173372ab5d349c23104b7ca02d6b
kayo <kayohramos@gmail.com>
2024-05-23 13:43:59 -0300

    query update

---
+++ BigQuery/automobile_data /car_info_queries.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT fuel_type FROM cars.car_info;
+"""
+Esta query seleciona todos os tipos de combustível (fuel_type) presentes na tabela car_info.
+palavra-chave DISTINCT para garantir que apenas valores únicos sejam retornados.
+"""
+
+SELECT MIN(length) AS min_length, MAX(length) AS max_length FROM cars.car_info;
+
+"""
+Calcula o comprimento mínimo e máximo (length) de todos os registros na tabela car_info
+Utiliza as funções agregadas MIN e MAX para encontrar, respectivamente, o menor e o maior valor na coluna length.
+"""
---
+++ BigQuery/automobile_data /update_table.sql
@@ -0,0 +1,16 @@
+SELECT * FROM cars.car_info
+WHERE num_of_doors IS NULL;
+
+"""Seleciona todos os registros da tabela car_info onde a coluna num_of_doors é NULL."""
+
+UPDATE cars.car_info 
+SET num_of_doors = "four" 
+WHERE make = "dodge" AND fuel_type = "gas" AND body_style = "sedan";
+
+
+"""
+Atualiza a coluna num_of_doors para o valor "four" para todos os registros na tabela car_info que atendem aos seguintes critérios:
+A coluna make é igual a "dodge".
+A coluna fuel_type é igual a "gas".
+A coluna body_style é igual a "sedan".
+"""
